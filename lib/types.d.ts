62a1066fbab16316f34c8b9e1da34a780cf16c9a
Alptuğ <alptugidin@gmail.com>
2022-12-11 05:35:31 +0300

    add new types

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,3 +1,17 @@
 export interface ICircularProgressBar {
    value: number
+   showValue?: boolean
+   additionalText?: string
+   barColor: string
+   barWidth: number
+   text?: {
+      valueSize?: number
+      additionalTextSize?: number
+      weight?: FontWeight
+      additionalTextWeight?: FontWeight
+      color?: string
+      additionalTextColor?: string
+   }
 }
+
+type FontWeight = 'normal' | 'bold' | 'bolder' | 'lighter'
