583c7539f819a167d6da5733b5ad959a1ba86559
Alptuğ <alptugidin@gmail.com>
2022-12-12 05:31:01 +0300

    add new types

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,17 +1,24 @@
-export interface ICircularProgressBar {
-   value: number
+export interface IFlat {
+   progress: number
    showValue?: boolean
    additionalText?: string
-   barColor: string
-   barWidth: number
-   text?: {
+   sx: {
+      barColor: string
+      barWidth: number
+      transitionTime?: number
       valueSize?: number
       additionalTextSize?: number
       weight?: FontWeight
       additionalTextWeight?: FontWeight
       color?: string
       additionalTextColor?: string
+      bgColor?: string
+      bgOpacity?: number
    }
+}
+
+export interface IHeat {
+   progress: number
 }
 
 type FontWeight = 'normal' | 'bold' | 'bolder' | 'lighter'
