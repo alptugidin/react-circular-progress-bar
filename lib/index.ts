f9a6205c5e5fe7f98009549171955c51342b6301
Alptuğ <alptugidin@gmail.com>
2022-12-12 05:30:55 +0300

    add new comps

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,3 @@
-import CircularProgressBar from './components/CircularProgressBar/CircularProgressBar';
-
-export default CircularProgressBar;
+import Flat from './components/CircularProgressBar/Flat';
+import Heat from './components/CircularProgressBar/Heat';
+export { Flat, Heat };
