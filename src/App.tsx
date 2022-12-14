4530b648a0d175931ae31a4813e669e72efea8dd
Alptuğ <alptugidin@gmail.com>
2022-12-14 05:19:24 +0300

    add new states to testing purposes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { FlatCanvas } from '../lib';
 import FlatDemo from './components/FlatDemo/FlatDemo';
 import HeatDemo from './components/HeatDemo/HeatDemo';
 const App: React.FC = () => {
+  const [progress, setProgress] = useState(100);
+  const [width, setWidth] = useState(200);
+  const [radiusRatio, setRadiusRatio] = useState(0);
   return (
     <div className='p-2 flex gap-10'>
       <FlatDemo/>
