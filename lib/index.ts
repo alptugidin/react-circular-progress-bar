142f5ce0793d17fbb9faa877ccc79b40186f2d15
Alptuğ <alptugidin@gmail.com>
2022-12-15 05:43:53 +0300

    update imports and exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
 import Flat from './components/CircularProgressBar/Flat';
 import Heat from './components/CircularProgressBar/Heat';
-import FlatCanvas from './components/CircularProgressBar/FlatCanvas';
-export { Flat, Heat, FlatCanvas };
+import FlatPath from './components/CircularProgressBar/FlathPath';
+export { Flat, Heat, FlatPath };
