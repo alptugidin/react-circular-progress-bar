34530dca41f064fb2f157b8d7bea9e9d794a3dcc
Alptuğ <alptugidin@gmail.com>
2022-12-16 05:46:35 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatPath } from '../lib';
+import CodeHighlighter from './components/CodeHighlighter/CodeHighlighter';
 import FlatDemo from './components/FlatDemo/FlatDemo';
 import FlatPathDemo from './components/FlatPathDemo/FlatPathDemo';
 import HeatDemo from './components/HeatDemo/HeatDemo';
+import { template } from './features/jsTemplate';
 const App: React.FC = () => {
   const [progress, setProgress] = useState(100);
   const [width, setWidth] = useState(200);
   const [radiusRatio, setRadiusRatio] = useState(0);
   return (
     <div className='p-2 flex gap-10'>
-      <FlatDemo/>
-      <HeatDemo/>
+      {/* <FlatDemo/> */}
+      {/* <HeatDemo/> */}
+      <div className='bg-[#282C34] w-1/2'>
+        <CodeHighlighter>
+          {template}
+        </CodeHighlighter>
+      </div>
     </div>
   );
 };
