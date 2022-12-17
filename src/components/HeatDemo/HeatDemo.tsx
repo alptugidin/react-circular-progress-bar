b02efdd5f397d0be7265b9f1a4b6d1efc44ff870
Alptuğ <alptugidin@gmail.com>
2022-12-17 05:49:36 +0300

    fix props and default values

--- src/components/HeatDemo/HeatDemo.tsx
+++ src/components/HeatDemo/HeatDemo.tsx
@@ -7,22 +7,22 @@ const HeatDemo: React.FC = () => {
   const [progress, setProgress] = useState(75);
   const [heatOptions, setHeatOptions] = useState<IHeatOptions>(
     {
-      strokeColor: '#ff0000',
       strokeWidth: 5,
-      bgColor: '#ffffff',
       valueSize: 30,
-      textSize: 15,
-      valueWeight: 'lighter',
-      textWeight: 'lighter',
-      valueFamily: 'Goudy Bookletter 1911',
-      textFamily: 'Goudy Bookletter 1911',
+      textSize: 14,
+      textWeight: 'normal',
+      valueWeight: 'normal',
+      valueFamily: 'Trebuchet MS',
+      textFamily: 'Trebuchet MS',
       valueColor: '#000000',
       textColor: '#000000',
       showValue: true,
       showText: true,
       revertColor: false,
       loadingTime: 1000,
-      text: 'Lorem, ipsum.'
+      text: 'Lorem, ipsum.',
+      strokeLinecap: 'round',
+      bgColor: '#ffffff'
     }
   );
   return (
@@ -30,20 +30,23 @@ const HeatDemo: React.FC = () => {
       <div className='w-48'>
         <Heat
           progress={progress}
-          showValue
+          showValue={heatOptions.showValue}
+          showText={heatOptions.showText}
           text={heatOptions.text}
           revertColor={heatOptions.revertColor}
           sx={{
-            barColor: heatOptions.strokeColor,
             barWidth: heatOptions.strokeWidth,
             bgColor: heatOptions.bgColor,
             valueSize: heatOptions.valueSize,
             textSize: heatOptions.textSize,
+            valueFamily: heatOptions.valueFamily,
+            textFamily: heatOptions.textFamily,
             valueWeight: heatOptions.valueWeight,
             textWeight: heatOptions.textWeight,
             textColor: heatOptions.textColor,
             valueColor: heatOptions.valueColor,
-            loadingTime: heatOptions.loadingTime
+            loadingTime: heatOptions.loadingTime,
+            strokeLinecap: heatOptions.strokeLinecap
           }}
         />
       </div>
