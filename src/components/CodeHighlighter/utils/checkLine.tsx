602ec856b0ee2c880d88bf48c00e2d4a6140ddf0
Alptuğ <alptugidin@gmail.com>
2022-12-17 05:48:56 +0300

    remove

--- src/components/CodeHighlighter/utils/checkLine.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export const checkLine = (line: string): JSX.Element => {
-  const output = <span>{line}</span>;
-  // if (line.includes('var ')) {
-  // console.log(line);
-  // }
-  return output;
-};
