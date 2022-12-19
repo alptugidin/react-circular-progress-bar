8891710d22811def2ca7f18f5dcbc567a0fc58c4
Alptuğ <alptugidin@gmail.com>
2022-12-19 05:25:42 +0300

    add shape and conditional props

--- lib/components/CircularProgressBar/Flat.tsx
+++ lib/components/CircularProgressBar/Flat.tsx
@@ -22,12 +22,57 @@ const Flat: React.FC<IFlat> = ({
     textWeight = 'lighter',
     loadingTime = 500,
     bgColor = '#ffffff',
-    strokeLinecap = 'butt'
+    strokeLinecap = 'butt',
+    shape = 'full'
   } = sx;
 
+  const setShape = (): number => {
+    switch (shape) {
+    case 'full':
+      return 100;
+    case 'threequarters':
+      return 75;
+    case 'half':
+      return 50; ;
+    }
+  };
+
+  const setRotate = (): string => {
+    switch (shape) {
+    case 'full':
+      return 'rotate(-90, 55, 55)';
+    case 'threequarters':
+      return 'rotate(135, 55, 55)';
+    case 'half':
+      return 'rotate(180, 55, 55)'; ;
+    }
+  };
+
+  const setRatio = (): number => {
+    switch (shape) {
+    case 'full':
+      return 1;
+    case 'threequarters':
+      return 0.75;
+    case 'half':
+      return 0.5; ;
+    }
+  };
+
+  const setBaseline = (): string => {
+    switch (shape) {
+    case 'full':
+      return 'rotate(-90, 55, 55)';
+    case 'threequarters':
+      return 'rotate(135, 55, 55)';
+    case 'half':
+      return 'rotate(180, 55, 55)'; ;
+    }
+  };
+
   useEffect(() => {
-    setAfterProgress(progress);
-  }, [progress]);
+    setAfterProgress(progress * setRatio());
+  }, [progress, shape]);
 
   const dasharray = 2 * Math.PI * 50;
   const dashoffset = (1 - (afterProgress + range.from) / range.to) * dasharray;
@@ -40,7 +85,7 @@ const Flat: React.FC<IFlat> = ({
           r="50"
           className='transition-effect'
           strokeWidth={sx.barWidth}
-          transform='rotate(-90 55 55)'
+          transform={setRotate()}
           fill="none"
           stroke={sx.barColor}
           shapeRendering='geometricPrecision'
@@ -49,26 +94,29 @@ const Flat: React.FC<IFlat> = ({
           strokeDashoffset={dashoffset}
         />
         {showValue &&
-        <text x="50%" y="50%"
+        <text
+          x="50%"
+          y={shape === 'half' ? '40%' : '50%' }
           fontSize={valueSize}
           fontWeight={valueWeight}
           textAnchor='middle'
           fontFamily={valueFamily}
           fill={valueColor}
         >
-          <tspan alignmentBaseline={showText ? 'baseline' : 'central'}>
+          <tspan alignmentBaseline={showText ? 'auto' : 'central'}>
             {progress}%
           </tspan>
         </text>}
         {showText &&
-        <text x="50%" y="50%"
+        <text
+          x="50%"
+          y={shape === 'half' ? '40%' : '50%' }
           fontSize={textSize}
           fontWeight={textWeight}
           textAnchor='middle'
           fill={textColor}
-          fontFamily={textFamily}
-          dominantBaseline={showValue ? 'hanging' : 'start'} >
-          <tspan alignmentBaseline={showValue ? 'hanging' : 'central'}>
+          fontFamily={textFamily}>
+          <tspan alignmentBaseline={showValue ? 'hanging' : (shape === 'half' ? 'hanging' : 'middle')}>
             {text}
           </tspan>
         </text>
@@ -82,6 +130,10 @@ const Flat: React.FC<IFlat> = ({
           fill="none"
           stroke={bgColor}
           strokeWidth={sx.barWidth - 0.3}
+          strokeDasharray={dasharray}
+          strokeLinecap={strokeLinecap}
+          strokeDashoffset={(1 - setShape() / 100) * dasharray}
+          transform={setRotate()}
           shapeRendering='geometricPrecision'
         />
       </svg>
