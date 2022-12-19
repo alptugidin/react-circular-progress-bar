83b4f56bffb0f50050957e1fbcdee1ab2d8db8b5
Alptuğ <alptugidin@gmail.com>
2022-12-19 05:25:20 +0300

    remove unused components

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,3 @@
 import Flat from './components/CircularProgressBar/Flat';
 import Heat from './components/CircularProgressBar/Heat';
-import FlatPath from './components/CircularProgressBar/FlathPath';
-export { Flat, Heat, FlatPath };
+export { Flat, Heat };
