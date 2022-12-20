ed717fcefa771ac7bb50a8506e03f5ad80c3aaa2
Alptuğ <alptugidin@gmail.com>
2022-12-20 05:00:09 +0300

    add nested

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import FlatDemo from './components/FlatDemo/FlatDemo';
 import HeatDemo from './components/HeatDemo/HeatDemo';
+import NestedDemo from './components/NestedDemo/NestedDemo';
 const App: React.FC = () => {
   return (
     <div className='p-2 flex gap-10'>
       <FlatDemo/>
       <HeatDemo/>
+      <NestedDemo/>
+      {/* Gradient */}
     </div>
   );
 };
