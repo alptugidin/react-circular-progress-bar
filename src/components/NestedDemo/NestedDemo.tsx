4026f7889a60b14434196341daa04d19123a8bc6
Alptuğ <alptugidin@gmail.com>
2022-12-20 05:02:14 +0300

    first

---
+++ src/components/NestedDemo/NestedDemo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Nested } from '../../../lib';
+
+const NestedDemo: React.FC = () => {
+  return (
+    <div className='w-48'>
+      <Nested/>
+    </div>
+  );
+};
+
+export default NestedDemo;
