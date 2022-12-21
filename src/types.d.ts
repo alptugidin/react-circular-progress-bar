2fce982f26b38af64dbeca42c85d0bb4ad6e32fc
Alptuğ <alptugidin@gmail.com>
2022-12-21 05:40:52 +0300

    create new interfaces for Nested

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export interface IHeatOptions {
    text: string
    strokeWidth: number
@@ -48,11 +50,33 @@ export interface IFlatOptions {
 }
 
 export interface IFlatSettings {
-   progress: number
    flatOptions: IFlatOptions
    setFlatOptions: React.Dispatch<React.SetStateAction<IFlatOptions>>
    progress: number
    setProgress: React.Dispatch<React.SetStateAction<number>>
+}
+
+interface IProgress {
+      circle1: number
+      circle2: number
+      circle3: number
+      circle4: number
+      circle5: number
+}
+export interface INestedOptions {
+   progress: IProgress
+   circle1color: string
+   circle2color: string
+   circle3color: string
+   circle4color: string
+   circle5color: string
+}
+
+export interface INestedSettings {
+   nestedOptions: INestedOptions
+   setNestedOptions: React.Dispatch<React.SetStateAction<INestedOptions>>
+   // progress: IProgress
+   // setProgress: React.Dispatch<React.SetStateAction<IProgress>>
 }
 
 type FontWeight = 'normal' | 'bold' | 'bolder' | 'lighter'
