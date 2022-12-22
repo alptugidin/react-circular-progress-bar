129ac58afae78fb3bdc0ea82d72a722d87c64767
Alptuğ <alptugidin@gmail.com>
2022-12-22 05:43:45 +0300

    create circles with loops

--- lib/components/CircularProgressBar/Nested.tsx
+++ lib/components/CircularProgressBar/Nested.tsx
@@ -9,203 +9,89 @@ const Nested: React.FC<INested> = ({
   const [afterProgress, setAfterProgress] = useState(
     { circle1: 0, circle2: 0, circle3: 0, circle4: 0, circle5: 0 }
   );
+  circles.sort((a: any, b: any) => b.value - a.value);
   const c1p = 50;
   const c2p = 40;
   const c3p = 30;
   const c4p = 20;
   const c5p = 10;
+  const cp = [c1p, c2p, c3p, c4p, c5p];
+  const textGap = [7, 18, 29, 40, 51];
   const dashoffset1 = (1 - (afterProgress.circle1 * 0.75) / 100) * 2 * Math.PI * c1p;
-  const dashoffset2 = (1 - (afterProgress.circle2 * 0.75 ?? 0) / 100) * 2 * Math.PI * c2p;
-  const dashoffset3 = (1 - (afterProgress.circle3 * 0.75 ?? 0) / 100) * 2 * Math.PI * c3p;
-  const dashoffset4 = (1 - (afterProgress.circle4 * 0.75 ?? 0) / 100) * 2 * Math.PI * c4p;
-  const dashoffset5 = (1 - (afterProgress.circle5 * 0.75 ?? 0) / 100) * 2 * Math.PI * c5p;
+  const dashoffset2 = (1 - (afterProgress.circle2 * 0.75) / 100) * 2 * Math.PI * c2p;
+  const dashoffset3 = (1 - (afterProgress.circle3 * 0.75) / 100) * 2 * Math.PI * c3p;
+  const dashoffset4 = (1 - (afterProgress.circle4 * 0.75) / 100) * 2 * Math.PI * c4p;
+  const dashoffset5 = (1 - (afterProgress.circle5 * 0.75) / 100) * 2 * Math.PI * c5p;
+  const dashoffsets = [dashoffset1, dashoffset2, dashoffset3, dashoffset4, dashoffset5];
   const strokeWidth = 6;
-  const fontsize = 5.5;
+  const fontsize = 6;
+  const fonntWeight = 'bold';
   useEffect(() => {
     setAfterProgress({
-
-      circle1: circles.circle1.value,
-      circle2: circles.circle2.value,
-      circle3: circles.circle3?.value ?? -1,
-      circle4: circles.circle4?.value ?? -1,
-      circle5: circles.circle5?.value ?? -1
+      circle1: circles[0].value,
+      circle2: circles[1].value,
+      circle3: circles[2] !== undefined ? circles[2].value : -1,
+      circle4: circles[3] !== undefined ? circles[3].value : -1,
+      circle5: circles[4] !== undefined ? circles[4].value : -1
     });
   }, [circles]);
 
   return (
     <div className='relative'>
-      <svg id='circle1' viewBox='0 0 110 110' className=''>
-
-        {Object.entries(circles).map((c, i) => (
-          <svg key={i} id='bg' className=' absolute top-0'>
-            <circle
-              cx={55}
-              cy={55}
-              r={50 - (10 * i)}
-              fill='none'
-              stroke='#cbd5e1'
-              strokeWidth={6}
-              strokeDasharray={2 * Math.PI * (50 - 10 * i)}
-              strokeDashoffset={(1 - (75) / 100) * 2 * Math.PI * (50 - 10 * i)}
-              transform='rotate(-90, 55, 55)'
-              strokeLinecap='round'
-            />
-          </svg>
-        ))}
-        <svg id='circle1' viewBox='0 0 110 110' className='absolute top-0'>
-          <circle
-            cx='55'
-            cy='55'
-            r={c1p}
-            fill='none'
-            transform='rotate(-90, 55, 55)'
-            strokeDasharray={2 * Math.PI * c1p}
-            strokeDashoffset={dashoffset1}
-            stroke={sx?.circle1Color}
-            style={{
-              transition: 'stroke-dashoffset ease-in-out',
-              transitionDuration: '1000ms'
-            }}
-            strokeWidth={strokeWidth}
-            strokeLinecap={strokeLinecap}
-          >
-          </circle>
+      <svg id='texts' viewBox='0 0 55 55' width={'46%'} className='absolute'>
+        {circles.map((circle, i) => (
+          circles[i]?.value !== -1 &&
           <text
-            x='45%'
-            y='1.5'
-            alignmentBaseline='hanging'
-            textAnchor='end'
+            key={i}
+            x='55'
+            y={textGap[i]}
             fontSize={fontsize}
-            fontWeight='bold'
-            fill='black'
-          >
-            {circles.circle1.text} {circles.circle1.value}%
+            fontWeight={fonntWeight}
+            textAnchor='end'>
+            {circle?.text} {circle?.value}%
           </text>
-        </svg>
+        ))}
       </svg>
-      <svg id='circle2' viewBox='0 0 110 110' className='absolute top-0'>
-        <circle
-          cx='55'
-          cy='55'
-          r={c2p}
-          fill='none'
-          style={{
-            transition: 'stroke-dashoffset ease-in-out',
-            transitionDuration: '1000ms'
-          }}
-          transform='rotate(-90, 55, 55)'
-          strokeDasharray={2 * Math.PI * c2p}
-          strokeDashoffset={dashoffset2}
-          stroke={sx?.circle2Color}
-          strokeWidth={strokeWidth}
-          strokeLinecap={strokeLinecap}
-        >
-        </circle>
-        <text
-          x='45%'
-          y='11.5'
-          alignmentBaseline='hanging'
-          textAnchor='end'
-          fontSize={fontsize}
-          fontWeight='bold'
-        >
-          {circles.circle2.text} {circles.circle2.value}%
-        </text>
+      <svg id='bg' viewBox='0 0 110 110'>
+        {circles.map((circle, i) => (
+          <g key={i}>
+            { circles[i]?.value !== -1 &&
+            <>
+              {/* background circles */}
+              <circle
+                cx='55'
+                cy='55'
+                r={50 - (10 * i)}
+                fill='none'
+                stroke='#cbd5e1'
+                strokeWidth={6}
+                strokeDasharray={2 * Math.PI * (50 - 10 * i)}
+                strokeDashoffset={(1 - (75) / 100) * 2 * Math.PI * (50 - 10 * i)}
+                transform='rotate(-90, 55, 55)'
+                strokeLinecap={strokeLinecap}
+              />
+              <circle
+                cx='55'
+                cy='55'
+                r={cp[i]}
+                style={{
+                  transition: 'stroke-dashoffset ease-in-out',
+                  transitionDuration: '1000ms'
+                }}
+                fill='none'
+                // className='transition-bg'
+                transform='rotate(-90, 55, 55)'
+                strokeDasharray={2 * Math.PI * cp[i]}
+                strokeDashoffset={dashoffsets[i]}
+                stroke={circle?.color}
+                strokeWidth={strokeWidth}
+                strokeLinecap={sx.strokeLinecap}
+              />
+            </>
+            }
+          </g>
+        ))}
       </svg>
-      {circles.circle3?.value !== -1 &&
-      <svg id='circle3' viewBox='0 0 110 110' className='absolute top-0'>
-        <circle
-          cx='55'
-          cy='55'
-          r={c3p}
-          fill='none'
-          style={{
-            transition: 'stroke-dashoffset ease-in-out',
-            transitionDuration: '1000ms'
-          }}
-          transform='rotate(-90, 55, 55)'
-          strokeDasharray={2 * Math.PI * c3p}
-          strokeDashoffset={dashoffset3}
-          stroke={sx?.circle3Color}
-          strokeWidth={strokeWidth}
-          strokeLinecap={strokeLinecap}
-        >
-        </circle>
-        <text
-          x='45%'
-          y='21.5'
-          alignmentBaseline='hanging'
-          textAnchor='end'
-          fontSize={fontsize}
-          fontWeight='bold  '
-        >
-          {circles.circle3?.text} {circles.circle3?.value}%
-        </text>
-      </svg>
-      }
-      {circles.circle4?.value !== -1 &&
-      <svg id='circle4' viewBox='0 0 110 110' className='absolute top-0'>
-        <circle
-          cx='55'
-          cy='55'
-          r={c4p}
-          fill='none'
-          style={{
-            transition: 'stroke-dashoffset ease-in-out',
-            transitionDuration: '1000ms'
-          }}
-          transform='rotate(-90, 55, 55)'
-          strokeDasharray={2 * Math.PI * c4p}
-          strokeDashoffset={dashoffset4}
-          stroke={sx?.circle4Color}
-          strokeWidth={strokeWidth}
-          strokeLinecap={strokeLinecap}
-        >
-        </circle>
-
-        <text
-          x='45%'
-          y='31.5'
-          alignmentBaseline='hanging'
-          textAnchor='end'
-          fontSize={fontsize}
-          fontWeight='bold'
-        >
-          {circles.circle4?.text} {circles.circle4?.value}%
-        </text>
-      </svg>
-      }
-      {circles.circle5?.value !== -1 &&
-      <svg id='circle5' viewBox='0 0 110 110' className='absolute top-0'>
-        <circle
-          cx='55'
-          cy='55'
-          r={c5p}
-          fill='none'
-          style={{
-            transition: 'stroke-dashoffset ease-in-out',
-            transitionDuration: '1000ms'
-          }}
-          transform='rotate(-90, 55, 55)'
-          strokeDasharray={2 * Math.PI * c5p}
-          strokeDashoffset={dashoffset5}
-          stroke={sx?.circle5Color}
-          strokeWidth={strokeWidth}
-          strokeLinecap={strokeLinecap}
-        >
-        </circle>
-        <text
-          x='45%'
-          y='41.5'
-          alignmentBaseline='hanging'
-          textAnchor='end'
-          fontSize={fontsize}
-          fontWeight='bold'
-        >
-          {circles.circle5?.text} {circles.circle5?.value}%
-        </text>
-      </svg>
-      }
     </div>
   );
 };
