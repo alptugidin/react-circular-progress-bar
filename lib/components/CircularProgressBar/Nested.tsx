0c817c1864c7e2f6b7288aff2692b5682bd13cf5
Alptuğ <alptugidin@gmail.com>
2022-12-23 04:55:08 +0300

    add props

--- lib/components/CircularProgressBar/Nested.tsx
+++ lib/components/CircularProgressBar/Nested.tsx
@@ -5,10 +5,11 @@ const Nested: React.FC<INested> = ({
   circles,
   sx
 }) => {
-  const { strokeLinecap = 'round' } = sx;
+  const { strokeLinecap = 'round', bgColor = '#cbd5e1', fontWeight = 'bold', fontFamily = 'Trebuchet MS' } = sx;
   const [afterProgress, setAfterProgress] = useState(
     { circle1: 0, circle2: 0, circle3: 0, circle4: 0, circle5: 0 }
   );
+
   circles.sort((a: any, b: any) => b.value - a.value);
   const c1p = 50;
   const c2p = 40;
@@ -16,7 +17,9 @@ const Nested: React.FC<INested> = ({
   const c4p = 20;
   const c5p = 10;
   const cp = [c1p, c2p, c3p, c4p, c5p];
-  const textGap = [7, 18, 29, 40, 51];
+  // const textGap = [7, 18, 29, 40, 51];
+  // const textGap = [7.5, 18.5, 29, 40, 51];
+  const textGap = [8.5, 19.5, 31.5, 43.5, 54.5];
   const dashoffset1 = (1 - (afterProgress.circle1 * 0.75) / 100) * 2 * Math.PI * c1p;
   const dashoffset2 = (1 - (afterProgress.circle2 * 0.75) / 100) * 2 * Math.PI * c2p;
   const dashoffset3 = (1 - (afterProgress.circle3 * 0.75) / 100) * 2 * Math.PI * c3p;
@@ -24,8 +27,7 @@ const Nested: React.FC<INested> = ({
   const dashoffset5 = (1 - (afterProgress.circle5 * 0.75) / 100) * 2 * Math.PI * c5p;
   const dashoffsets = [dashoffset1, dashoffset2, dashoffset3, dashoffset4, dashoffset5];
   const strokeWidth = 6;
-  const fontsize = 6;
-  const fonntWeight = 'bold';
+  const fontsize = 7;
   useEffect(() => {
     setAfterProgress({
       circle1: circles[0].value,
@@ -38,7 +40,7 @@ const Nested: React.FC<INested> = ({
 
   return (
     <div className='relative'>
-      <svg id='texts' viewBox='0 0 55 55' width={'46%'} className='absolute'>
+      <svg id='texts' viewBox='0 0 55 60' width={'43%'} className='absolute transition-all'>
         {circles.map((circle, i) => (
           circles[i]?.value !== -1 &&
           <text
@@ -46,7 +48,8 @@ const Nested: React.FC<INested> = ({
             x='55'
             y={textGap[i]}
             fontSize={fontsize}
-            fontWeight={fonntWeight}
+            fontWeight={fontWeight}
+            fontFamily={fontFamily}
             textAnchor='end'>
             {circle?.text} {circle?.value}%
           </text>
@@ -56,30 +59,29 @@ const Nested: React.FC<INested> = ({
         {circles.map((circle, i) => (
           <g key={i}>
             { circles[i]?.value !== -1 &&
-            <>
+            <g>
               {/* background circles */}
               <circle
                 cx='55'
                 cy='55'
                 r={50 - (10 * i)}
                 fill='none'
-                stroke='#cbd5e1'
+                stroke={bgColor}
                 strokeWidth={6}
                 strokeDasharray={2 * Math.PI * (50 - 10 * i)}
                 strokeDashoffset={(1 - (75) / 100) * 2 * Math.PI * (50 - 10 * i)}
                 transform='rotate(-90, 55, 55)'
                 strokeLinecap={strokeLinecap}
               />
+              {/* progress circles */}
               <circle
                 cx='55'
                 cy='55'
                 r={cp[i]}
                 style={{
-                  transition: 'stroke-dashoffset ease-in-out',
-                  transitionDuration: '1000ms'
+                  transition: 'stroke 100ms, stroke-dashoffset 1000ms ease-in-out'
                 }}
                 fill='none'
-                // className='transition-bg'
                 transform='rotate(-90, 55, 55)'
                 strokeDasharray={2 * Math.PI * cp[i]}
                 strokeDashoffset={dashoffsets[i]}
@@ -87,7 +89,7 @@ const Nested: React.FC<INested> = ({
                 strokeWidth={strokeWidth}
                 strokeLinecap={sx.strokeLinecap}
               />
-            </>
+            </g>
             }
           </g>
         ))}
