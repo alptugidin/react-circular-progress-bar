d92bb0bd0841ccb9c629210b55034c494055a13b
Alptuğ <alptugidin@gmail.com>
2022-12-25 05:12:55 +0300

    remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ import HeatDemo from './components/HeatDemo/HeatDemo';
 import NestedDemo from './components/NestedDemo/NestedDemo';
 const App: React.FC = () => {
   return (
-    <div className='p-2 flex  gap-10 justify-center'>
+    <div className='p-2 flex gap-10 justify-center'>
       <FlatDemo/>
       <HeatDemo/>
       <NestedDemo/>
-      {/* Gradient */}
     </div>
   );
 };
