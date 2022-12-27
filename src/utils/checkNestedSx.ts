9ebd513f0e7ee2f38dfd9a29d1d86565a6989464
Alptuğ <alptugidin@gmail.com>
2022-12-27 05:26:04 +0300

    conditional rendering for nested's 'code'

---
+++ src/utils/checkNestedSx.ts
@@ -0,0 +1,25 @@
+import { INestedSettings } from '../types';
+
+export const checkNestedSx = (props: INestedSettings): string[] => {
+  const arr: string[] = [];
+  if (props.nestedOptions.fontWeight !== 'bold') {
+    arr.push(`\t\tfontWeight: '${props.nestedOptions.fontWeight}'`);
+  }
+  if (props.nestedOptions.fontFamily !== 'Trebuchet MS') {
+    arr.push(`\t\tfontFamily: '${props.nestedOptions.fontFamily}'`);
+  }
+  if (props.nestedOptions.strokeLinecap !== 'round') {
+    arr.push(`\t\tstrokeLinecap: '${props.nestedOptions.strokeLinecap}'`);
+  }
+  if (!props.nestedOptions.valueAnimation) {
+    arr.push(`\t\tvalueAnimation: ${props.nestedOptions.valueAnimation ? 'true' : 'false'}`);
+  }
+  if (!props.nestedOptions.intersectionEnabled) {
+    arr.push(`\t\tintersectionEnabled: ${props.nestedOptions.intersectionEnabled ? 'true' : 'false'}`);
+  }
+  if (props.nestedOptions.loadingTime !== 1000) {
+    arr.push(`\t\tloadingTime: ${props.nestedOptions.loadingTime}`);
+  }
+  arr.push(`\t\tbgColor: '${props.nestedOptions.background}'`);
+  return arr;
+};
