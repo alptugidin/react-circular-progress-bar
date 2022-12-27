98e731fe2decbd485d7bf1b28015c7b754064fa0
Alptuğ <alptugidin@gmail.com>
2022-12-27 05:25:54 +0300

    conditional rendering for heat's 'code'

---
+++ src/utils/checkHeatProps.ts
@@ -0,0 +1,25 @@
+import { IHeatSettings } from '../types';
+
+export const checkHeatProps = (props: IHeatSettings): string[] => {
+  const arr: string[] = [];
+  if (props.heatOptions.range.to !== 100 && props.heatOptions.range.from !== 0) {
+    arr.push(`\trange={{ from: ${props.heatOptions.range.from}, to: ${props.heatOptions.range.to} }}`);
+  }
+  if (props.heatOptions.sign.value !== '%' || props.heatOptions.sign.position !== 'end') {
+    arr.push(`\tsign={{ value: '${props.heatOptions.sign.value}', position: '${props.heatOptions.sign.position}' }}`);
+  }
+
+  if (props.heatOptions.text !== undefined && props.heatOptions.text !== '') {
+    arr.push(`\ttext={'${props.heatOptions.text}'}`);
+  }
+  if (!props.heatOptions.showValue) {
+    arr.push(`\tshowValue={ ${props.heatOptions.showValue ? 'true' : 'false'} }`);
+  }
+
+  if (props.heatOptions.revertColor) {
+    arr.push(`\trevertColor={${props.heatOptions.revertColor ? 'true' : 'false'}}`);
+  }
+
+  arr.push(`\tprogress={${props.progress}}`);
+  return arr;
+};
