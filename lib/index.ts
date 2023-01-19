229dbc2d37b77ec7d478d81839bc705d312fb195
Alptuğ <alptugidin@gmail.com>
2023-01-20 00:22:42 +0300

    add dist

--- lib/index.ts
+++
@@ -1,4 +0,0 @@
-import Flat from './components/CircularProgressBar/Flat';
-import Heat from './components/CircularProgressBar/Heat';
-import Nested from './components/CircularProgressBar/Nested';
-export { Flat, Heat, Nested };
